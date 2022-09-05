d53b41d3f4043a9b00e76c1918f02a42b9902e64
Tbence98 <tlrbence@gmail.com>
2022-09-05 22:34:17 +0200

    Remove unnecessary media queries

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const snake = document.getElementsByClassName("snake");
-const snakeSize = snake[0].clientWidth;                                         // snake items always has the same width and height
+const snakeSize = snake[0].clientWidth; // snake items always has the same width and height
 const map = document.querySelector(".map");
 const foodContainer = document.querySelector(".food-container");
 const pointsElement = document.querySelector(".points");
 const recordElement = document.querySelector(".record");
 const mapWidth = map.clientWidth;
 const mapHeigth = map.clientHeight;
-const foodContainerSize = foodContainer.clientWidth;                            // food container always has the same width and height
+const foodContainerSize = foodContainer.clientWidth; // food container always has the same width and height
 let randomfoodX = 0;
 let randomfoodY = 0;
 let currentVector = "a";
@@ -16,34 +16,33 @@ let gameSpeed = 300;
 
 alert("Control: w: forward, d: right, a: left, s: down");
 
- if (!localStorage.getItem("record")) {
-            localStorage.setItem("record", points);
-        }
+if (!localStorage.getItem("record")) {
+    localStorage.setItem("record", points);
+}
 recordElement.textContent = `Record: ${localStorage.getItem("record")}`;
 
 // Initial values, the items contains info about each snake element
-const snakeInfos = [ 
-    // ====== Snake head =======                                                           
+const snakeInfos = [
+    // First item is the snake head
     {
-        "snakeVector": "a",
-        "offsetX": mapWidth / 2,
-        "offsetY": mapHeigth / 2
-    },
-    // =========================
-    {
-        "snakeVector": "a",
-        "offsetX": mapWidth / 2 + snakeSize,
-        "offsetY": mapHeigth / 2
+        snakeVector: "a",
+        offsetX: mapWidth / 2,
+        offsetY: mapHeigth / 2,
     },
     {
-        "snakeVector": "a",
-        "offsetX": mapWidth / 2 + snakeSize * 2,
-        "offsetY": mapHeigth / 2
+        snakeVector: "a",
+        offsetX: mapWidth / 2 + snakeSize,
+        offsetY: mapHeigth / 2,
     },
     {
-        "snakeVector": "a",
-        "offsetX": mapWidth / 2 + snakeSize * 3,
-        "offsetY": mapHeigth / 2
+        snakeVector: "a",
+        offsetX: mapWidth / 2 + snakeSize * 2,
+        offsetY: mapHeigth / 2,
+    },
+    {
+        snakeVector: "a",
+        offsetX: mapWidth / 2 + snakeSize * 3,
+        offsetY: mapHeigth / 2,
     },
 ];
 
@@ -57,7 +56,6 @@ createFood();
 window.addEventListener("keydown", setSnakeHeadVector);
 
 function setSnakeHeadVector(e) {
-        
     switch (currentVector) {
         case "a": {
             if (e.key === "w" || e.key === "s") {
@@ -83,7 +81,7 @@ function setSnakeHeadVector(e) {
             }
             break;
         }
-    }  
+    }
 }
 
 function moveSnake() {
@@ -91,22 +89,22 @@ function moveSnake() {
         switch (snakeInfos[i].snakeVector) {
             case "a": {
                 snakeInfos[i].offsetX -= snakeSize;
-                snake[i].style.left = `${snakeInfos[i].offsetX}px`;               
+                snake[i].style.left = `${snakeInfos[i].offsetX}px`;
                 break;
             }
             case "d": {
                 snakeInfos[i].offsetX += snakeSize;
-                snake[i].style.left = `${snakeInfos[i].offsetX}px`;              
+                snake[i].style.left = `${snakeInfos[i].offsetX}px`;
                 break;
             }
             case "w": {
                 snakeInfos[i].offsetY -= snakeSize;
-                snake[i].style.top = `${snakeInfos[i].offsetY}px`;               
+                snake[i].style.top = `${snakeInfos[i].offsetY}px`;
                 break;
             }
             case "s": {
                 snakeInfos[i].offsetY += snakeSize;
-                snake[i].style.top = `${snakeInfos[i].offsetY}px`;              
+                snake[i].style.top = `${snakeInfos[i].offsetY}px`;
                 break;
             }
         }
@@ -115,22 +113,37 @@ function moveSnake() {
 }
 
 function updateVectors() {
-    for (let i = snakeInfos.length - 1; i > 0; i--) {                               
-        snakeInfos[i].snakeVector = snakeInfos[i-1].snakeVector;
+    for (let i = snakeInfos.length - 1; i > 0; i--) {
+        snakeInfos[i].snakeVector = snakeInfos[i - 1].snakeVector;
     }
 }
 
 function createFood() {
-    randomfoodX = Math.floor(Math.random() * (mapWidth / foodContainerSize - 1)) * foodContainerSize;
-    randomfoodY = Math.floor(Math.random() * (mapHeigth / foodContainerSize - 1)) * foodContainerSize;
-    let isInsideSnake = snakeInfos.some( element => element.offsetX === randomfoodX && element.offsetY === randomfoodY);
+    randomfoodX =
+        Math.floor(Math.random() * (mapWidth / foodContainerSize - 1)) *
+        foodContainerSize;
+    randomfoodY =
+        Math.floor(Math.random() * (mapHeigth / foodContainerSize - 1)) *
+        foodContainerSize;
+    let isInsideSnake = snakeInfos.some(
+        (element) =>
+            element.offsetX === randomfoodX && element.offsetY === randomfoodY
+    );
 
     // If the food position matches the snake position, than create new positions
     while (isInsideSnake) {
-        randomfoodX = Math.floor(Math.random() * (mapWidth / foodContainerSize - 1)) * foodContainerSize;
-        randomfoodY = Math.floor(Math.random() * (mapHeigth / foodContainerSize - 1)) * foodContainerSize;
-        isInsideSnake = snakeInfos.some( element => element.offsetX === randomfoodX && element.offsetY === randomfoodY);
-    }  
+        randomfoodX =
+            Math.floor(Math.random() * (mapWidth / foodContainerSize - 1)) *
+            foodContainerSize;
+        randomfoodY =
+            Math.floor(Math.random() * (mapHeigth / foodContainerSize - 1)) *
+            foodContainerSize;
+        isInsideSnake = snakeInfos.some(
+            (element) =>
+                element.offsetX === randomfoodX &&
+                element.offsetY === randomfoodY
+        );
+    }
 
     foodContainer.style.left = `${randomfoodX}px`;
     foodContainer.style.top = `${randomfoodY}px`;
@@ -139,38 +152,38 @@ function createFood() {
 function growing() {
     const newSnake = document.createElement("div");
     newSnake.classList.add("snake");
-        
+
     switch (snakeInfos[snakeInfos.length - 1].snakeVector) {
         case "a": {
             snakeInfos.push({
-                "snakeVector": snakeInfos[snakeInfos.length - 1].snakeVector,
-                "offsetX": snakeInfos[snakeInfos.length - 1].offsetX + snakeSize,
-                "offsetY": snakeInfos[snakeInfos.length - 1].offsetY
-            })
+                snakeVector: snakeInfos[snakeInfos.length - 1].snakeVector,
+                offsetX: snakeInfos[snakeInfos.length - 1].offsetX + snakeSize,
+                offsetY: snakeInfos[snakeInfos.length - 1].offsetY,
+            });
             break;
         }
         case "d": {
             snakeInfos.push({
-                "snakeVector": snakeInfos[snakeInfos.length - 1].snakeVector,
-                "offsetX": snakeInfos[snakeInfos.length - 1].offsetX - snakeSize,
-                "offsetY": snakeInfos[snakeInfos.length - 1].offsetY
-            })
+                snakeVector: snakeInfos[snakeInfos.length - 1].snakeVector,
+                offsetX: snakeInfos[snakeInfos.length - 1].offsetX - snakeSize,
+                offsetY: snakeInfos[snakeInfos.length - 1].offsetY,
+            });
             break;
         }
         case "w": {
             snakeInfos.push({
-                "snakeVector": snakeInfos[snakeInfos.length - 1].snakeVector,
-                "offsetX": snakeInfos[snakeInfos.length - 1].offsetX,
-                "offsetY": snakeInfos[snakeInfos.length - 1].offsetY + snakeSize
-            })
+                snakeVector: snakeInfos[snakeInfos.length - 1].snakeVector,
+                offsetX: snakeInfos[snakeInfos.length - 1].offsetX,
+                offsetY: snakeInfos[snakeInfos.length - 1].offsetY + snakeSize,
+            });
             break;
         }
         case "s": {
             snakeInfos.push({
-                "snakeVector": snakeInfos[snakeInfos.length - 1].snakeVector,
-                "offsetX": snakeInfos[snakeInfos.length - 1].offsetX,
-                "offsetY": snakeInfos[snakeInfos.length - 1].offsetY - snakeSize
-            })
+                snakeVector: snakeInfos[snakeInfos.length - 1].snakeVector,
+                offsetX: snakeInfos[snakeInfos.length - 1].offsetX,
+                offsetY: snakeInfos[snakeInfos.length - 1].offsetY - snakeSize,
+            });
             break;
         }
     }
@@ -180,22 +193,35 @@ function growing() {
 }
 
 function testFoodHit() {
-    return randomfoodX === snakeInfos[0].offsetX && randomfoodY === snakeInfos[0].offsetY;
+    return (
+        randomfoodX === snakeInfos[0].offsetX &&
+        randomfoodY === snakeInfos[0].offsetY
+    );
 }
 
 function testWallHit() {
-    return snakeInfos[0].offsetX < 0 || snakeInfos[0].offsetX >= mapWidth || snakeInfos[0].offsetY < 0 || snakeInfos[0].offsetY >= mapHeigth;
+    return (
+        snakeInfos[0].offsetX < 0 ||
+        snakeInfos[0].offsetX >= mapWidth ||
+        snakeInfos[0].offsetY < 0 ||
+        snakeInfos[0].offsetY >= mapHeigth
+    );
 }
 
 function testBodyHit() {
-   return snakeInfos.some( (element, index) => index > 0 && element.offsetX === snakeInfos[0].offsetX && element.offsetY === snakeInfos[0].offsetY);
+    return snakeInfos.some(
+        (element, index) =>
+            index > 0 &&
+            element.offsetX === snakeInfos[0].offsetX &&
+            element.offsetY === snakeInfos[0].offsetY
+    );
 }
 
 function getResult(isNewRecord) {
     document.body.innerHTML = `<div class="message-container">
         <div class="message-box">
             <h2 id="title">Your score:</h2>
-            <h1 class="score"><i>${points}</i> Pts.</h1>
+            <h1 class="score"><span>${points}</span> Pts.</h1>
             <button type="button" class="btn">TRY AGAIN</button>
         </div>
     </div>`;
@@ -213,7 +239,6 @@ function updatePoints() {
 }
 
 function runGame() {
-
     if (testFoodHit()) {
         growing();
         createFood();
@@ -221,28 +246,26 @@ function runGame() {
         if (points % 5 === 0) {
             if (gameSpeed > 100) {
                 gameSpeed -= 25;
-            }
-            else if (gameSpeed > 60) {
+            } else if (gameSpeed > 60) {
                 gameSpeed -= 10;
             }
-            clearInterval(gameInterval);           
+            clearInterval(gameInterval);
             gameInterval = setInterval(runGame, gameSpeed);
         }
     }
-    
+
     moveSnake();
-  
+
     if (testWallHit() || testBodyHit()) {
-        clearInterval(gameInterval);       
+        clearInterval(gameInterval);
         if (localStorage.getItem("record") < points) {
             localStorage.setItem("record", points);
             isNewRecord = true;
         }
         getResult(isNewRecord);
     }
-  
-    
- /* Every item get the vector value from the previous item,
+
+    /* Every item get the vector value from the previous item,
     but the first item get it from the user in the eventListener.
     This vector modify is important when the snake is changing direction. */
     updateVectors();
